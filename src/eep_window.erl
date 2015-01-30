3e1f6ed41bad984fbe6ac490f8eb14d5f6ef26c7
sanmiguel <michael.coles@gmail.com>
2015-01-30 20:24:09 +1300

    Reorder and rearrange functions: core func first, higher to lower level; process oriented helpers last

--- src/eep_window.erl
+++ src/eep_window.erl
@@ -47,6 +47,71 @@
          events = []
         }).
 
+%% Pure window functionality.
+
+%% Window type creation functions.
+%% TODO More window types/options to follow.
+tumbling(none, events, Size, Aggregate, Seed) ->
+    #win{type=tumbling, by=events, compensating=false,
+         size=Size, aggmod=Aggregate, agg=(Aggregate:init(Seed))}.
+
+sliding(none, events, Size, Aggregate, Seed) ->
+    #win{type=sliding, by=events, compensating=true,
+         size=Size, aggmod=Aggregate, agg=(Aggregate:init(Seed))}.
+
+%% Window command interface.
+push(Event, #win{type=sliding, by=events}=Win) ->
+    #win{count=Count, size=Size} = Win,
+    Actions = slide(Count, Size),
+    decide(Actions, Event, Win);
+push(Event, #win{type=tumbling, by=events}=Win) ->
+    #win{count=Count, size=Size} = Win,
+    Actions = tumble(Count, Size),
+    decide(Actions, Event, Win).
+
+%% Window behaviour functions: given the current relevant state of a window,
+%% pick what action(s) are next.
+tumble(Count, Size) when Count  < Size -> [accumulate];
+tumble(Count, Size) when Count >= Size -> [accumulate, emit, reset].
+
+slide(Count, Size) when Count  < Size -> [accumulate];
+slide(Count, Size) when Count == Size -> [accumulate, emit];
+slide(Count, Size) when Count  > Size -> [accumulate, compensate, emit].
+
+%% Window internal mechanisms: these correspond to the actions decided upon
+%% by the behaviour functions above.
+accumulate(Event, #win{aggmod=AMod, agg=Agg, count=Count}=Win) ->
+    Accumed = Win#win{agg=(AMod:accumulate(Agg, Event)), count=Count+1},
+    if Win#win.compensating -> %% If we're to compensate we must store the events
+           Accumed#win{events= Win#win.events++[Event]};
+       true ->
+           Accumed
+    end.
+
+compensate(#win{aggmod=AMod, agg=Agg, events=[Oldest|Es]}=Win) ->
+    Win#win{agg=(AMod:compensate(Agg, Oldest)), events=Es}.
+
+reset(#win{aggmod=AMod, seed=Seed}=Win) ->
+    Win#win{agg=(AMod:init(Seed)), count=1}.
+
+%% Given a list of actions (from the behaviour functions above), an event and
+%% the current window state, apply the actions in order and return the result.
+%% Its implementation is ~equivalent to lists:foldl/3 on the list of actions.
+decide(Actions, Event, Window) ->
+    decide(Actions, Event, Window, noop).
+
+decide([], _, Window, Decision) -> {Decision, Window};
+decide([accumulate|Actions], Event, Window, Decision) ->
+    decide(Actions, Event, accumulate(Event, Window), Decision);
+decide([compensate|Actions], Event, Window, Decision) ->
+    decide(Actions, Event, compensate(Window), Decision);
+decide([reset|Actions], Event, Window, Decision) ->
+    decide(Actions, Event, reset(Window), Decision);
+decide([emit|Actions], Event, Window, noop) ->
+    %% TODO This enforces only one emission per decision: is this right?
+    decide(Actions, Event, Window, {emit, Window#win.agg}).
+
+%% Process functionality: utils for running windows as a process.
 start(Window, AggMod, Interval) ->
     start(Window, AggMod, eep_clock_wall, Interval).
 start(Window, AggMod, ClockMod, Interval) ->
@@ -80,57 +145,3 @@ loop(Window, EventPid, State) ->
       From ! {debug, State},
       loop(Window, EventPid, State)
   end.
-
-
-tumbling(none, events, Size, Aggregate, Seed) ->
-    #win{type=tumbling, by=events, compensating=false,
-         size=Size, aggmod=Aggregate, agg=(Aggregate:init(Seed))}.
-
-sliding(none, events, Size, Aggregate, Seed) ->
-    #win{type=sliding, by=events, compensating=true,
-         size=Size, aggmod=Aggregate, agg=(Aggregate:init(Seed))}.
-
-push(Event, #win{type=sliding, by=events}=Win) ->
-    #win{count=Count, size=Size} = Win,
-    Actions = slide(Count, Size),
-    decide(Actions, Event, Win);
-push(Event, #win{type=tumbling, by=events}=Win) ->
-    #win{count=Count, size=Size} = Win,
-    Actions = tumble(Count, Size),
-    decide(Actions, Event, Win).
-
-
-tumble(Count, Size) when Count  < Size -> [accumulate];
-tumble(Count, Size) when Count >= Size -> [accumulate, emit, reset].
-
-slide(Count, Size) when Count  < Size -> [accumulate];
-slide(Count, Size) when Count == Size -> [accumulate, emit];
-slide(Count, Size) when Count  > Size -> [accumulate, compensate, emit].
-
-accumulate(Event, #win{aggmod=AMod, agg=Agg, count=Count}=Win) ->
-    Accumed = Win#win{agg=(AMod:accumulate(Agg, Event)), count=Count+1},
-    if Win#win.compensating -> %% If we're to compensate we must store the events
-           Accumed#win{events= Win#win.events++[Event]};
-       true ->
-           Accumed
-    end.
-
-compensate(#win{aggmod=AMod, agg=Agg, events=[Oldest|Es]}=Win) ->
-    Win#win{agg=(AMod:compensate(Agg, Oldest)), events=Es}.
-
-reset(#win{aggmod=AMod, seed=Seed}=Win) ->
-    Win#win{agg=(AMod:init(Seed)), count=1}.
-
-decide(Actions, Event, Window) ->
-    decide(Actions, Event, Window, noop).
-
-decide([], _, Window, Decision) -> {Decision, Window};
-decide([accumulate|Actions], Event, Window, Decision) ->
-    decide(Actions, Event, accumulate(Event, Window), Decision);
-decide([compensate|Actions], Event, Window, Decision) ->
-    decide(Actions, Event, compensate(Window), Decision);
-decide([reset|Actions], Event, Window, Decision) ->
-    decide(Actions, Event, reset(Window), Decision);
-decide([emit|Actions], Event, Window, noop) ->
-    %% TODO This enforces only one emission per decision: is this right?
-    decide(Actions, Event, Window, {emit, Window#win.agg}).
